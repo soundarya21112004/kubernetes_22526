a64a8e89c54006e137187bd3f06c32da6c22bd35
soundarya21112004 <soundarya21112004@gmail.com>
2026-05-27 11:25:36 +0530

    changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,36 +1,29 @@
-# 1. Kubernetes Master Node
+# modules/ec2/main.tf
+
 resource "aws_instance" "master" {
-  ami                    = "ami-0522ab6e1ddcc7055" # Updated Ubuntu 22.04 LTS AMI
-  instance_type          = "t2.medium"             
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.medium"
+  key_name      = "k8s-jenkins-key"  # 👈 Changed here
 
   tags = {
     Name = "k8s-master"
   }
 }
 
-# 2. Kubernetes Worker Node 1
 resource "aws_instance" "worker1" {
-  ami                    = "ami-0522ab6e1ddcc7055" # Updated Ubuntu 22.04 LTS AMI
-  instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.micro"
+  key_name      = "k8s-jenkins-key"  # 👈 Changed here
 
   tags = {
     Name = "k8s-worker-1"
   }
 }
 
-# 3. Kubernetes Worker Node 2
 resource "aws_instance" "worker2" {
-  ami                    = "ami-0522ab6e1ddcc7055" # Updated Ubuntu 22.04 LTS AMI
-  instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.micro"
+  key_name      = "k8s-jenkins-key"  # 👈 Changed here
 
   tags = {
     Name = "k8s-worker-2"
--- terraform/modules/keypair/main.tf
+++ terraform/modules/keypair/main.tf
@@ -1,18 +1,17 @@
+# modules/keypair/main.tf
+
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "generated" {
-  key_name   = "k8s-key"
+  key_name   = "k8s-jenkins-key"  # 👈 Changed from "k8s-key"
   public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "local_file" "private_key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "${path.module}/k8s-key.pem"
-
-  provisioner "local-exec" {
-    command = "chmod 400 ${path.module}/k8s-key.pem"
-  }
+  content         = tls_private_key.rsa.private_key_pem
+  filename        = "modules/keypair/k8s-jenkins-key.pem" # 👈 Updated filename too
+  file_permission = "0400"
 }
