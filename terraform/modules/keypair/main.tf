407ed7bc4bc02694ae7e6056587df46336495114
soundarya21112004 <soundarya21112004@gmail.com>
2026-05-27 12:17:59 +0530

    changes

--- terraform/modules/keypair/main.tf
+++ terraform/modules/keypair/main.tf
@@ -8,7 +8,13 @@ resource "tls_private_key" "rsa" {
 resource "aws_key_pair" "generated" {
   key_name   = "k8s-jenkins-key"
   public_key = tls_private_key.rsa.public_key_openssh
+
+  # Avoid failing Jenkins on subsequent runs if keypair already exists
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 }
+
 
 resource "local_file" "private_key" {
   content         = tls_private_key.rsa.private_key_pem
